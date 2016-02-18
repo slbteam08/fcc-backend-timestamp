4909dcb3c7e7532cea6f4f3e5ddbb6c97560742d
Eric Wong <slbteam08@hotmail.com>
2016-02-18 18:48:19 +0000

    Complete the functions

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 var http = require("http");
+var url = require("url");
+var moment = require("moment");
 
 var server = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("Hello World");
+  var info = url.parse(req.url, true);
+  
+  if (info.pathname == "/") {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Please enter a date string or unix timestamp");
+  } else {
+    var param = decodeURIComponent(info.pathname.substr(1));
+    var dateLang = moment(new Date(param));
+    var dateUnix = moment.unix(param);
+    var result = null;
+    
+    if (dateUnix.isValid()) {
+      result = dateUnix;
+    } else if (dateLang.isValid()) {
+      result = dateLang;
+    }
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    if (result === null) {
+      res.end(JSON.stringify({
+  	    unix: null,
+  	    natural: null
+  	  })); 
+    } else {
+      res.end(JSON.stringify({
+  	    unix: result.unix(),
+  	    natural: result.format("MMMM D, YYYY")
+  	  })); 
+    }
+  }
 });
 server.listen(process.env.PORT, process.env.IP, 50, function(err, data) {
   if (err) throw err
