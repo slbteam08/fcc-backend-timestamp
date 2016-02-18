dba14bde8a58460fa1ab755d1c295b2d87de7ca8
Eric Wong <slbteam08@hotmail.com>
2016-02-18 17:29:33 +0000

    Adjust Hello World

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var http = require("http");
 
 var server = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end("Hello World");
 });
-server.listen(80);
+server.listen(process.env.PORT, process.env.IP, 50, function(err, data) {
+  if (err) throw err
+  console.log("Listening on: "+process.env.IP+":"+process.env.PORT)
+});
